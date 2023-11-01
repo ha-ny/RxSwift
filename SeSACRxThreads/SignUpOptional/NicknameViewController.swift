ad06172298a6136e218f41d46e1c5a81ba3cb5b5
ha-ny <hany01.dev@gmail.com>
2023-11-01 20:35:05 +0900

    Initial Commit

---
+++ SeSACRxThreads/SignUpOptional/NicknameViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NicknameViewController.swift
+//  SeSACRxThreads
+//
+//  Created by jack on 2023/10/30.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class NicknameViewController: UIViewController {
+
+    var nickName = BehaviorSubject(value: "")
+    var isNextButtonHidden = BehaviorSubject(value: true)
+    let disposeBag = DisposeBag()
+    
+    let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
+    let nextButton = PointButton(title: "다음")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Color.white
+        configureLayout()
+        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+
+        bind()
+        
+    }
+    
+    func bind() {
+        nicknameTextField.rx.text.orEmpty.bind(to: nickName).disposed(by: disposeBag)
+        isNextButtonHidden.bind(to: nextButton.rx.isHidden).disposed(by: disposeBag)
+
+        nickName.map{ $0.count > 2 && $0.count < 6 }
+            .subscribe(with: self) { owner, value in
+                owner.isNextButtonHidden.onNext(!value)
+            }.disposed(by: disposeBag)
+    }
+    
+    @objc func nextButtonClicked() {
+        navigationController?.pushViewController(BirthdayViewController(), animated: true)
+    }
+
+    
+    func configureLayout() {
+        view.addSubview(nicknameTextField)
+        view.addSubview(nextButton)
+         
+        nicknameTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+
+}
